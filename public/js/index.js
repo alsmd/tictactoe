8abd92b095a886e7405ef3461e14b5336805a14e
alsmd <dangerveneta@gmail.com>
2022-04-23 11:03:58 -0300

    play again button

--- public/js/index.js
+++ public/js/index.js
@@ -2,16 +2,22 @@ let gameInfo;
 let starter = document.querySelector("#starter");
 let gameStart = false;
 starter.addEventListener("click", function(event) {
-    fetch("/create").then(function(response) {
+    newGame();
+})
+
+
+async function newGame() {
+    await fetch("/create").then(function(response) {
         return (response.json());
     }).then(tryEnterGame);
-})
+}
 
 async function tryEnterGame(response) {
     let loading = document.querySelector("#loading");
     starter.style.display = "none"
     loading.style.display = "block"
     let r;
+    gameInfo = response;
     while (1) {
         r = await fetch("/check-game", {
             headers: {
@@ -19,20 +25,20 @@ async function tryEnterGame(response) {
             },
             method: "POST",
             body: JSON.stringify({
-                gameInfo: response
+                gameInfo
             })
         }).then((r) => r.json());
         if (r.start == true)
             break;
     }
 
-    gameInfo = response;
     start_game(gameInfo)
 }
 
 async function start_game(gameInfo) {
     let loading = document.querySelector("#loading");
     loading.style.display = "none";
+    updateBoard();
     let game = document.querySelector("#game");
     game.style.display = "block";
     gameStart = true;
@@ -67,6 +73,24 @@ for (let i = 0; i < buttons.length; i++) {
     })
 }
 
+//play again
+let playAgain = document.querySelector("#play-again");
+playAgain.addEventListener("click", async function(event) {
+    drawTabuleiro([
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ]);
+    let victory = document.querySelector("#victory");
+    victory.style.display = "none"
+    let gameBoard = document.querySelector("#game");
+    event.target.style.display = "none"
+    gameBoard.style.display = "none"
+    newGame();
+});
+
+
+
 
 //ATUALIZAR EM TEMPO REAL
 
@@ -76,14 +100,17 @@ function drawTabuleiro(tabuleiro) {
     for (var i = 0; i < fields.length; i++) {
         let y = Math.trunc(i / 3);
         let x = i - (y * 3);
-        if (tabuleiro[x][y] != '0')
+        if (tabuleiro[x][y] == '0')
+            fields[i].textContent = "";
+        else
             fields[i].textContent = tabuleiro[x][y];
     }
 }
 
 async function updateBoard() {
-    if (!gameStart)
+    if (!gameStart) {
         return;
+    }
     let response = await fetch('/get-board', {
         headers: {
             'Content-Type': 'application/json',
@@ -92,8 +119,11 @@ async function updateBoard() {
         body: JSON.stringify({ id: gameInfo.id, player: gameInfo.player })
     }).then((r) => r.json());
     let tabuleiro = response.tabuleiro;
+    console.log(tabuleiro)
     drawTabuleiro(tabuleiro);
     if (!response.has_connection) {
+        let playAgain = document.querySelector("#play-again");
+        playAgain.style.display = "block";
         let victory = document.querySelector("#victory");
         victory.style.display = "block"
         victory.textContent = "Player " + (gameInfo.player == 1 ? "2" : "1") + " foi desconectado!"
@@ -102,14 +132,18 @@ async function updateBoard() {
         return;
     }
     if (response.win != false) {
+        let playAgain = document.querySelector("#play-again");
+        playAgain.style.display = "block";
         let victory = document.querySelector("#victory");
         victory.style.display = "block"
-        if (gameInfo.player == 1 && response.win == "X")
+        if (gameInfo.player == 1 && response.win == "X") {
             victory.textContent = response.win + ", você venceu!"
-        else if (gameInfo.player == 2 && response.win == "O")
+            victory.style.background = "green";
+        } else if (gameInfo.player == 2 && response.win == "O") {
+            victory.style.background = "green";
             victory.textContent = response.win + ", você venceu!"
-        else {
-            victory.textContent = gameInfo.player == 1 ? "X" : "O" + ", você perdeu!"
+        } else {
+            victory.textContent = (gameInfo.player == 1 ? "X" : "O") + ", você perdeu!"
             victory.style.background = "red";
         }
 
